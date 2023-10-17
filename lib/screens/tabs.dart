314659204dcd3253bc02151bf2b6d1af226f1e79
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-10-17 13:46:30 +0200

    add tabs

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int selectedPageIndex = 0;
+
+  void selectPage(int index) {
+    setState(() {
+      selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen();
+    var activePageTitle = "Categories";
+
+    if (selectedPageIndex == 1) {
+      activePage = MealsScreen(meals: []);
+      activePageTitle = "Favourites";
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          selectPage(index);
+        },
+        currentIndex: selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: "Categories"),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favourites"),
+        ],
+      ),
+    );
+  }
+}
